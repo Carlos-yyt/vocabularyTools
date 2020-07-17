e4bd9211bbe0d1195e9d3ebc861e1bed2b96e255
Carlos <302642469@qq.com>
2020-07-18 05:44:57 +0800

    外来词库导入数据库生成个人生词词典
    数据库使用SQLite

--- src/database.py
+++ src/database.py
@@ -1,9 +1,14 @@
+import os
 import sqlite3
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def create_dictionaries(user_name, src_type, src_file_url):
     """
     根据用户提供的单词表创建一个该用户的生词表
+    todo:支持多种格式的来源，目前支持的格式为：每词三行，单词音标中文各占一行每词三行，单词音标中文各占一行。
     :param user_name:用户的名称
     :param src_type:用户提供的单词表的类型
     :param src_file_url:用户提供的单词表的文件位置
@@ -19,7 +24,7 @@ def create_dictionaries(user_name, src_type, src_file_url):
     拓展：遇到的次数、
     """
     sql_exec = '''
-    CREATE TABLE words(
+    CREATE TABLE IF NOT EXISTS words(
     word    TEXT    PRIMARY KEY,
     translation  TEXT,
     IPA VARCHAR(255),
@@ -28,5 +33,23 @@ def create_dictionaries(user_name, src_type, src_file_url):
     )
     '''
     curs.execute(sql_exec)
+
+    """逐行导入数据库"""
+    with open(src_file_url, encoding='utf-8') as file_object:
+        for index, line in enumerate(file_object):  # 逐行读取
+            if index % 3 == 0:
+                _word = line.strip('\n')  # 第一列：单词
+                logging.debug(_word)
+            elif index % 3 == 1:
+                _translation = line.strip('\n')  # 第二列：释义
+                logging.debug(_translation)
+            else:
+                _ipa = line.strip('\n')  # 第三列：国际音标
+                logging.debug(_ipa)
+                _audio = './audio/' + _word + '.mp3'  # 第四列：音频文件地址
+                logging.debug(_audio)
+                sql_exec = "INSERT OR IGNORE INTO words VALUES (?,?,?,?,?)"  # ignore:防止用户提供的题库有重复单词
+                curs.execute(sql_exec, (_word, _translation, _ipa, _audio, 0))  # 遇到的次数 初始为0
+
     conn.commit()
     conn.close()
--- src/test.py
+++ src/test.py
@@ -4,4 +4,4 @@ from src import dictionaries
 from src import database
 
 #dictionaries.txt2excel(os.path.abspath(r'data\测试词库.txt'), os.path.abspath('audio'))
-database.create_dictionaries("Carlos", "我爱背单词", "file_url")
+database.create_dictionaries("Carlos", "我爱背单词", r'data\测试词库.txt')
